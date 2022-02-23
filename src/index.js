1a1064bc97b8a9fde189a83d3b94c1cd11387c00
sebski123 <sebthomle@gmail.com>
2022-02-23 22:54:27 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import reducer from "./reducer";
+
+const store = createStore(reducer);
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: "GOOD",
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button>ok</button>
+      <button>bad</button>
+      <button>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok</div>
+      <div>bad</div>
+    </div>
+  );
+};
+
+const renderApp = () => {
+  ReactDOM.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+renderApp();
+store.subscribe(renderApp);
